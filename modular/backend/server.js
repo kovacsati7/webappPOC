30dd2e671d3cd179a0c8cab0af4b043b46467613
kovacsati7 <kovacsati7@yahoo.com>
2016-09-07 09:48:21 +0300

    Updated to working version

--- modular/backend/server.js
+++ modular/backend/server.js
@@ -8,9 +8,10 @@ var userData = [
 ];
 
 app.use(function(req,res,next){
-  res.header('Acces-Control-Allow-Origin','*');
-  res.header('Acces-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
-})
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', function (req,res) {
   res.send('Hello World Backend!');
